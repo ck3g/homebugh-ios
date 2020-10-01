18a24ad4d56828799788436c5876bcf7f96c8c39
Vitali Tatarintev <vtatarintev@gitlab.com>
2020-10-01 09:10:35 +0200

    Create a blank app

---
+++ HomeBugh/HomeBughApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeBughApp.swift
+//  HomeBugh
+//
+//  Created by Vitali Tatarintev on 01.10.20.
+//
+
+import SwiftUI
+
+@main
+struct HomeBughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

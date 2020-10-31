4a9b437da2117d52d99ae0f10a7c5ee3cfd1122b
Nataly <ngranetchi@gmail.com>
2020-10-31 17:46:00 +0100

    Delete transaction

--- HomeBugh/TransactionsView.swift
+++ HomeBugh/TransactionsView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 class Transactions: ObservableObject {
-  @Published var items = [
-    Transaction(date: "October 20, 2020", amount: "18,23$", category: "Sport", categoryType: "Spending", account: "Deutsche Bank", comment: "Magnesium"),
-    Transaction(date: "October 20, 2020", amount: "5$", category: "Food", categoryType: "Spending", account: "Bar", comment: "")
-]
+    @Published var items = [
+        Transaction(date: "October 20, 2020", amount: "18,23$", category: "Sport", categoryType: "Spending", account: "Deutsche Bank", comment: "Magnesium"),
+        Transaction(date: "October 20, 2020", amount: "5$", category: "Food", categoryType: "Spending", account: "Bar", comment: "")
+    ]
 }
 
 struct TransactionsView: View {
@@ -19,11 +19,30 @@ struct TransactionsView: View {
     @ObservedObject var transactions = Transactions()
     
     @State private var addTransactionViewVisible = false
+    @State private var showingAlert = false
+    @State private var deleteIndexSet: IndexSet?
     
     var body: some View {
         NavigationView {
-            List(transactions.items) { transaction in
-                TransactionCell(transaction: transaction)
+            List {
+                ForEach(transactions.items, id: \.self) { transaction in
+                    TransactionCell(transaction: transaction)
+                }
+                .onDelete(perform: { indexSet in
+                    self.showingAlert = true
+                    self.deleteIndexSet = indexSet
+                })
+
+                .alert(isPresented: self.$showingAlert) {
+                    let indexSet = self.deleteIndexSet!
+                    return Alert(
+                        title: Text("Delete transaction"),
+                        message: Text("Are you sure you want to delete the transaction?"),
+                        primaryButton: .default(Text("Yes")) {
+                            self.transactions.items.remove(atOffsets: indexSet)
+                        },
+                        secondaryButton: .cancel())
+                }
             }
             .navigationBarTitle("Transactions")
             .navigationBarItems(trailing:
@@ -37,6 +56,10 @@ struct TransactionsView: View {
         .sheet(isPresented: $addTransactionViewVisible) {
             AddTransactionView(transactions: self.transactions)
         }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        transactions.items.remove(atOffsets: offsets)
     }
 }
 

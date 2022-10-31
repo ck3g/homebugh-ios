920bcbff5b65e88b380248ae679d2a8b4dba3811
Nataly <ngranetchi@gmail.com>
2022-10-31 13:42:46 +0100

    Hide add new transaction button

--- HomeBugh/Screens/TransactionsView/TransactionsView.swift
+++ HomeBugh/Screens/TransactionsView/TransactionsView.swift
@@ -41,13 +41,13 @@ struct TransactionsView: View {
                 }
             }
             .navigationTitle("Transactions")
-            .navigationBarItems(trailing:
-                                    Button(action: {
-                                        self.addTransactionViewVisible = true
-                                    }) {
-                                        Image(systemName: "plus")
-                                    }
-            )
+//            .navigationBarItems(trailing:
+//                                    Button(action: {
+//                                        self.addTransactionViewVisible = true
+//                                    }) {
+//                                        Image(systemName: "plus")
+//                                    }
+//            )
         }
         .navigationViewStyle(StackNavigationViewStyle())
         .sheet(isPresented: $addTransactionViewVisible) {
